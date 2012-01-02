3191b8529983db8cbd65fb08abc7a128bc642540
Gøran Hansen <mail@goeran.no>
2012-01-02 17:40:41 +0100

    are able to scrape data for both a private person and a company from gulesider. Mechanize is doing the jobb well :)

--- spec/learning/to_scrape_gulesider_spec.rb
+++ spec/learning/to_scrape_gulesider_spec.rb
@@ -1,15 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "learning to scrape gulesider.no" do
-  it "should be able to scrape data from page?" do
-    gulesider = "http://www.gulesider.no/finn:"
-    agent = Mechanize.new
-    result = agent.get gulesider + "90582054"
+  before :each do
+    @gulesider = "http://www.gulesider.no/finn:"
+    @agent = Mechanize.new
+  end
+  
+  it "should be possible to scrape data from page?" do
+    result = @agent.get @gulesider + "90582054"
     list = result.search "#result-list"
     first_hit = result.search "div.hit"
     phone = first_hit.search("li.tel:first-child").text;
     name = first_hit.search("div.header").search("a").text
     puts "Phone: " + phone
     puts "Name: " + name.to_s
+  end
+  
+  it "should be possible to scrape data for a private person?" do
+    result = @agent.get @gulesider + "98260555"
+    #http://www.gulesider.no/person/profil/stian+eliassen/0000808649400000000
+    #puts @agent.current_page.canonical_uri
+    vcard = result.search "div.vcard"
+    given_name = vcard.search("span.given-name").text
+    family_name = vcard.search("span.family-name").text
+    name = "#{given_name} #{family_name}"
+    address = vcard.search "span.adr"
+    street_name = address.search("span.street-address").text
+    postal_code = address.search("span.postal-code").text
+    city = address.search("span.locality").text
+    latitude = address.search("span.latitude").text
+    longitude = address.search("span.longitude").text
+    puts "Name: " + name
+    puts "Adresse: " + street_name
+    puts "Postnummer: " + postal_code
+    puts "By: " + city
+    puts "Lat/Long: #{latitude}, #{longitude}"
   end
 end
