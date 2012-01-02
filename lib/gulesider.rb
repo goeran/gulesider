481ef91599be94d2c3be7bf2b95fc0f450e628dc
Gøran Hansen <mail@goeran.no>
2012-01-02 19:11:56 +0100

    refactored the code to make it more readable

--- lib/gulesider.rb
+++ lib/gulesider.rb
@@ -1,37 +1,9 @@
 require 'mechanize'
+require 'screen_scraper'
 require 'search_result'
 
 class Gulesider
-  def self.search number 
-    gulesider = "http://www.gulesider.no/finn:"
-    agent = Mechanize.new
-    result = agent.get gulesider + number
-    
-    if agent.current_page.canonical_uri.to_s.include? "http://www.gulesider.no/person" 
-      vcard = result.search "div.vcard"
-      address = vcard.search "span.adr"
-      return SearchResult.new({
-        :person => true,
-        :name => vcard.search("span.given-name").text + " " + vcard.search("span.family-name").text,
-        :phone => number,
-        :street_name => address.search("span.street-address").text,
-        :postal_code => address.search("span.postal-code").text,
-        :city => address.search("span.locality").text,
-        :latitude => address.search("span.latitude").text,
-        :longitude => address.search("span.longitude").text
-      })
-    else
-      list = result.search "#result-list"
-      first_hit = result.search "div.hit"
-      
-      return SearchResult.new({
-        :company => true,
-        :name => first_hit.search("div.header").search("a").text,
-        :phone => first_hit.search("li.tel:first-child").text,
-        :street_name => first_hit.search("span.street-address").text,
-        :postal_code => first_hit.search("span.postal-code").text,
-        :city => first_hit.search("span.locality").text
-      })
-    end
+  def self.search number
+    return ScreenScraper.new.search number
   end
 end
---
+++ lib/screen_scraper.rb
@@ -0,0 +1,52 @@
+class ScreenScraper
+  def initialize
+    @gulesider = "http://www.gulesider.no/finn:"
+    @agent = Mechanize.new
+  end
+  
+  def search number
+    search_webpage_for number
+    
+    return scraped_person_data if number_belongs_to_a_person?
+    return scraped_company_data
+  end
+  
+  private
+  def search_webpage_for number
+    @result = @agent.get @gulesider + number
+  end
+  
+  def number_belongs_to_a_person?
+    @agent.current_page.canonical_uri.to_s.include? "http://www.gulesider.no/person"
+  end
+  
+  def scraped_person_data
+    vcard = @result.search "div.vcard"
+    address = vcard.search "span.adr"
+    
+    return SearchResult.new({
+      :person => true,
+      :name => vcard.search("span.given-name").text + " " + vcard.search("span.family-name").text,
+      :phone => vcard.search("li.tel:first-child").text,
+      :street_name => address.search("span.street-address").text,
+      :postal_code => address.search("span.postal-code").text,
+      :city => address.search("span.locality").text,
+      :latitude => address.search("span.latitude").text,
+      :longitude => address.search("span.longitude").text
+    })
+  end
+  
+  def scraped_company_data
+    list = @result.search "#result-list"
+    first_hit = list.search "div.hit"
+    
+    return SearchResult.new({
+      :company => true,
+      :name => first_hit.search("div.header").search("a").text,
+      :phone => first_hit.search("li.tel:first-child").text,
+      :street_name => first_hit.search("span.street-address").text,
+      :postal_code => first_hit.search("span.postal-code").text,
+      :city => first_hit.search("span.locality").text
+    })
+  end
+end
