5d10fc7fbaf3e403f117a85ad94864e20f686e0e
Gøran Hansen <mail@goeran.no>
2012-01-02 18:03:21 +0100

    refactored the search into a method

--- spec/learning/to_scrape_gulesider_spec.rb
+++ spec/learning/to_scrape_gulesider_spec.rb
@@ -1,45 +1,53 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe "learning to scrape gulesider.no" do
-  before :each do
-    @gulesider = "http://www.gulesider.no/finn:"
-    @agent = Mechanize.new
+  it "should be possible to scrape data from page?" do
+    result = search "90582054"
+
+    puts "Phone: " + result[:phone]
+    puts "Name: " + result[:name]
+    puts "Adresse: " + result[:street_name]
+    puts "Postnummer: " + result[:postal_code]
+    puts "By: " + result[:city]
   end
   
-  it "should be possible to scrape data from page?" do
-    result = @agent.get @gulesider + "90582054"
-    list = result.search "#result-list"
-    first_hit = result.search "div.hit"
-    phone = first_hit.search("li.tel:first-child").text;
-    name = first_hit.search("div.header").search("a").text
-    street_name = first_hit.search("span.street-address").text
-    postal_code = first_hit.search("span.postal-code").text
-    city = first_hit.search("span.locality").text
-    puts "Phone: " + phone
-    puts "Name: " + name.to_s
-    puts "Adresse: " + street_name
-    puts "Postnummer: " + postal_code
-    puts "By: " + city
+  def search number 
+    gulesider = "http://www.gulesider.no/finn:"
+    agent = Mechanize.new
+    result = agent.get gulesider + number
+    
+    if agent.current_page.canonical_uri.to_s.include? "http://www.gulesider.no/person" 
+      vcard = result.search "div.vcard"
+      address = vcard.search "span.adr"
+      return {
+        :name => vcard.search("span.given-name").text + " " + vcard.search("span.family-name").text,
+        :phone => number,
+        :street_name => address.search("span.street-address").text,
+        :postal_code => address.search("span.postal-code").text,
+        :city => address.search("span.locality").text,
+        :latitude => address.search("span.latitude").text,
+        :longitude => address.search("span.longitude").text
+      }
+    else
+      list = result.search "#result-list"
+      first_hit = result.search "div.hit"
+      
+      return {
+        :name => first_hit.search("div.header").search("a").text,
+        :phone => first_hit.search("li.tel:first-child").text,
+        :street_name => first_hit.search("span.street-address").text,
+        :postal_code => first_hit.search("span.postal-code").text,
+        :city => first_hit.search("span.locality").text
+      }
+    end
   end
   
   it "should be possible to scrape data for a private person?" do
-    result = @agent.get @gulesider + "98260555"
-    #http://www.gulesider.no/person/profil/stian+eliassen/0000808649400000000
-    #puts @agent.current_page.canonical_uri
-    vcard = result.search "div.vcard"
-    given_name = vcard.search("span.given-name").text
-    family_name = vcard.search("span.family-name").text
-    name = "#{given_name} #{family_name}"
-    address = vcard.search "span.adr"
-    street_name = address.search("span.street-address").text
-    postal_code = address.search("span.postal-code").text
-    city = address.search("span.locality").text
-    latitude = address.search("span.latitude").text
-    longitude = address.search("span.longitude").text
-    puts "Name: " + name
-    puts "Adresse: " + street_name
-    puts "Postnummer: " + postal_code
-    puts "By: " + city
-    puts "Lat/Long: #{latitude}, #{longitude}"
+    result = search "98260555"
+    puts "Name: " + result[:name]
+    puts "Adresse: " + result[:street_name]
+    puts "Postnummer: " + result[:postal_code]
+    puts "By: " + result[:city]
+    puts "Lat/Long: " + result[:latitude] + ", " + result[:longitude]
   end
 end
